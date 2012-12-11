2c606ab7578a3860017295dcb02c2ffcb506f4d8
waxspin <ben@irq5.net>
2012-12-11 17:15:35 -0600

    Added white pins, also setting up timer code.

--- HexColor/HexColor.ino
+++ HexColor/HexColor.ino
@@ -1,16 +1,26 @@
 int RED_PIN = 6;
 int GREEN_PIN = 5;
 int BLUE_PIN = 3;
+int WHITE_PIN_1 = 10;
+int WHITE_PIN_2 = 11;
+
+int oldMillis = 0;
+int nowMillis = 0;
 
 
 void setup() {
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
+  pinMode(WHITE_PIN_1, OUTPUT);
+  pinMode(WHITE_PIN_2, OUTPUT);
   
   analogWrite(RED_PIN, 255);
-    analogWrite(GREEN_PIN, 255);
-    analogWrite(BLUE_PIN, 255);
+  analogWrite(GREEN_PIN, 255);
+  analogWrite(BLUE_PIN, 255);
+  analogWrite(WHITE_PIN_1, 255);
+  analogWrite(WHITE_PIN_2, 255);
+  
   
   Serial.begin(9600);
   
