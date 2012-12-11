984de527fcf8f5f6258e91032b50ef820119d87f
waxspin <ben@irq5.net>
2012-12-11 17:08:26 -0600

    Setup and serial port initialization.

--- HexColor/HexColor.ino
+++ HexColor/HexColor.ino
@@ -1,5 +1,20 @@
+int RED_PIN = 6;
+int GREEN_PIN = 5;
+int BLUE_PIN = 3;
+
+
 void setup() {
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
   
+  analogWrite(RED_PIN, 255);
+    analogWrite(GREEN_PIN, 255);
+    analogWrite(BLUE_PIN, 255);
+  
+  Serial.begin(9600);
+  
+  Serial.println("The initialization has taken place.");
 }
 
 void loop() {
